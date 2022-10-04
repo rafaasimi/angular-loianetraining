17a972be7c237e56a277b93b79d37f998ccd1dac
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-10-04 13:54:14 -0300

    Aplicando validações iniciais no formulario

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
-const MODULES = [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FormsModule];
+const MODULES = [BrowserModule, AppRoutingModule, BrowserAnimationsModule, TemplateFormModule];
 const COMPONENTS = [AppComponent, DataFormComponent];
 
 @NgModule({
