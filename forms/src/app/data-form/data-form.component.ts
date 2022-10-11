4340d5fbc40deabf85772ec3be2835d9cf766559
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-10-11 09:39:58 -0300

    Aplicado método de reset no formulario

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,19 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.scss']
+  styleUrls: ['./data-form.component.scss'],
 })
 export class DataFormComponent implements OnInit {
-
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
-
     // 1ª FORMA DE CRIAR FORMULARIO
     // this.formulario = new FormGroup({
     //   nome: new FormControl('Rafael'),
@@ -24,8 +23,26 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null],
       email: [null],
-    })
-
+    });
   }
 
+  onSubmit() {
+    console.log(this.formulario.value);
+
+    this.http
+      .post('/enderecoServer/formUsuario',JSON.stringify(this.formulario.value))
+      .subscribe(
+        (response) => {
+          console.log(response);
+
+          // Reseta o formulário
+          this.resetarFormulario();
+        },
+        (error: any) => alert('Erro ao enviar o formulário.')
+      );
+  }
+
+  resetarFormulario() {
+    this.formulario.reset();
+  }
 }
--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DataFormComponent } from './data-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 const COMPONENTS = [DataFormComponent];
 
-const MODULES = [CommonModule, ReactiveFormsModule, SharedModule];
+const MODULES = [CommonModule, ReactiveFormsModule, SharedModule, HttpClientModule];
 
 @NgModule({
   declarations: [COMPONENTS],
