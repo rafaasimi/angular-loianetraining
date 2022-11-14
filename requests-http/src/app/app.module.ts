434076c3061597f416a2fce47902337b541aaab4
Rafael Simionato <rafaasimi@hotmail.com>
2022-11-14 20:15:10 -0300

    Criado estrutura inicial da tabela de cursos

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const COMPONENTES = [AppComponent];
+
+const MODULES = [BrowserModule, AppRoutingModule, HttpClientModule];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [COMPONENTES],
+  imports: [MODULES],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
