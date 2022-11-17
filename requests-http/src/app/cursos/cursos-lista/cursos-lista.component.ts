40c9f6db0afde693fc9b3fa04e51384488b2279e
Rafael Simionato <rafaasimi@hotmail.com>
2022-11-17 12:51:40 -0300

    Capurando error HTTP

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -7,19 +7,45 @@ import { CursosService } from '../cursos.service';
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
   styleUrls: ['./cursos-lista.component.scss'],
-  preserveWhitespaces: true
+  preserveWhitespaces: true,
 })
 export class CursosListaComponent implements OnInit {
-
   // cursos: Curso[] = [];
-  cursos$: Observable<Curso[]> | undefined
+  cursos$: Observable<Curso[]> | undefined;
+  error$ = new Subject<boolean>();
 
-  constructor(private cursosService: CursosService) { }
+  constructor(private cursosService: CursosService) {}
 
   ngOnInit(): void {
     // this.cursosService.list().subscribe(dados => this.cursos = dados);
 
-    this.cursos$ = this.cursosService.list()
+    this.onRefresh();
   }
 
+  onRefresh() {
+    // Maneira 1
+    this.cursos$ = this.cursosService.list().pipe(
+      catchError((error) => {
+        console.log(error);
+        this.error$.next(true);
+        return of();
+      })
+    );
+
+    // Maneira 2
+    this.cursosService
+      .list()
+      .pipe(
+        catchError((error) => {
+          console.log(error);
+          return of();
+        })
+      )
+      .subscribe(
+        (dados) => console.log(dados),
+        (error) => console.error(error),
+        () => console.log('Observable completo!')
+      );
+  }
+  
 }
